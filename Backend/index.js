8390d37f77b5c6b3ccf47df83571a1472ee6acb7
Sambhav Agarwal <sambhavagg123@gmail.com>
2025-02-18 20:00:54 +0530

    Final Update

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: `${process.env.FRONTEND_URL}`,
     credentials: true,
 }));
 
